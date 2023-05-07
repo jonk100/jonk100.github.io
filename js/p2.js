bf704bdd83b8563c403daf8796f3a364651d3130
jon <jon@theappleseeds.com>
2023-05-07 13:03:10 -0400

    blah

--- js/p2.js
+++ js/p2.js
@@ -96,23 +96,6 @@ $(document).ready(function () {
     });
 
     // ______________________________
-    /*    // Get the active song from p2.js
-    var activeSong =  $('.active-song').attr('data-src'); // the function that gets the active song
 
-    // Loop through all the tabs in the HTML
-    var tabs = document.querySelectorAll('.tabcontent');
-    tabs.forEach(function (tab) {
-
-    // Get the data-src attribute of the tab
-    var tabSrc = tab.getAttribute('data-song');
-
-    // If the tab's data-src attribute matches the active song, add the active class to the tab
-    if (tabSrc === activeSong) {
-        tab.classList.add('active');
-    } else {
-        // Otherwise, remove the active class from the tab
-        tab.classList.remove('active');
-    }
-    };*/
 
 });
--- js/tabs.js
+++ js/tabs.js
@@ -19,3 +19,27 @@ function openSong(evt, songName) {
   evt.currentTarget.className += " active";
 }
 
+
+  /*
+    // Get the active song from p2.js
+ function activeSong() {
+  $('.player__author').text($('.active-song').attr('data-author'));
+  $('.player__song').text($('.active-song').attr('data-song'));; // the function that gets the active song
+  }
+  // Loop through all the tabs in the HTML
+  var tabs = document.querySelectorAll('.tabcontent');
+  tabs.forEach(function (tab) {
+
+  // Get the data-src attribute of the tab
+  var tabSrc = tab.getAttribute('data-src');
+
+  // If the tab's data-src attribute matches the active song, add the active class to the tab
+  if (tabSrc === activeSong) {
+      tab.classList.add('active');
+  } else {
+      // Otherwise, remove the active class from the tab
+      tab.classList.remove('active');
+  }
+  })
+
+  */
